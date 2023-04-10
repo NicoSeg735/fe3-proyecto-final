873fbe7ee22df9649b9e38a22be1b26116d03c50
Nicolas Segurado <seguradol@globalhitss.com>
2023-04-09 22:20:19 -0300

    Font awesome added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,19 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { SessionContext } from '../contexts/Session'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons'
 
 const Card = ({ user }) => {
 	const { state, dispatch } = useContext(SessionContext)
 
 	return (
-		<div className='flex flex-col gap-2 w-full max-w-[500px] p-4 bg-slate-200 dark:bg-slate-800 rounded-lg cursor-pointer'>
-			<Link to={'/dentist/' + user.id}>
+		<Link
+			className='flex flex-col gap-2 w-full max-w-[500px] p-4 bg-slate-200 dark:bg-slate-800 rounded-lg cursor-pointer'
+			to={'/dentist/' + user.id}
+		>
+			<div className='flex items-center justify-between'>
 				<div className='flex items-center justify-start gap-2'>
 					<img
 						src='/img/doctor.jpg'
@@ -16,15 +22,26 @@ const Card = ({ user }) => {
 					/>
 					<span className='text-lg font-semibold'>{user.name}</span>
 				</div>
-			</Link>
-			<div
-				onClick={() =>
-					dispatch({ type: 'SWITCH_FAV', payload: user.id })
-				}
-			>
-				{state.favs.includes(user.id) ? 'Borrar' : 'Agregar'}
+				<div
+					className={
+						'flex w-fit p-1 duration-200 ' +
+						(state.favs.includes(user.id)
+							? 'text-red-400'
+							: 'text-slate-400')
+					}
+					onClick={e => {
+						e.preventDefault()
+						dispatch({ type: 'SWITCH_FAV', payload: user.id })
+					}}
+				>
+					{state.favs.includes(user.id) ? (
+						<FontAwesomeIcon icon={solidHeart} />
+					) : (
+						<FontAwesomeIcon icon={regularHeart} />
+					)}
+				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
